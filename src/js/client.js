943c4d42f623fdc1cc6bd93013bf42b42ec100b8
Soren2024 <jankie2023@outlook.com>
2024-02-11 14:51:29 +0800

    Update goto method

--- src/js/client.js
+++ src/js/client.js
@@ -14,7 +14,7 @@ sleep(2000);
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var res = JSON.paser(this.responseText);
-        window.open('https://mc.minecraftbox.link/-----' + res.domain);
+        window.location.href = "https://mc.minecraftbox.link/-----" + res.domain
     }
     xhttp.open("GET", "https://www.awaland.xyz/zlibrary/cn/message.json", true);
     xhttp.send();
