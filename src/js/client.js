6cb7ecaa79e49415c8063549cbd0f6d205372fa3
Soren2024 <jankie2023@outlook.com>
2024-02-11 14:44:57 +0800

    Update client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,11 +1,10 @@
 let lang = navigator.language;
-if(lang=="zh-CN"){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            // Todo: Parse return information
-        }
-    };
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var res = JSON.paser(this.responseText);
+        window.open('https://mc.minecraftbox.link/-----' + res.domain);
+    }
     xhttp.open("GET", "https://www.awaland.xyz/zlibrary/cn/message.json", true);
     xhttp.send();
 }
