24e73d333009d79cb56d0ee79d22e1b4dc5365bb
Soren2024 <jankie2023@outlook.com>
2024-02-11 14:46:16 +0800

    Add function sleep

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,13 @@
 let lang = navigator.language;
 var xhttp = new XMLHttpRequest();
+function sleep(n) {
+    var start = new Date().getTime();
+    while (true) {
+        if (new Date().getTime() - start > n) {
+            break;
+        }
+    }
+}
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var res = JSON.paser(this.responseText);
